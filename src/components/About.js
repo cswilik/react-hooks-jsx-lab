046d6f974969d15d3793990de1e2ff2aa084fe02
CSWILIK <cswilik@gmail.com>
2021-01-11 14:11:26 -0500

    commit tree a3eddd5ec7bed2133e41ef3a3bbeab5c80949260

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Caramels candy chocolate cake candy canes cupcake toffee jelly beans tootsie roll marshmallow. Brownie jujubes sesame snaps lollipop pie. Cheesecake wafer cupcake donut.</p>
+    <img src={image} alt= "I made this"/>
+  </div>;
 }
 
 export default About;
